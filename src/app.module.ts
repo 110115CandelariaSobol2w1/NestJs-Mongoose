2ff5160941622da6060aebaf38b67ad04404d812
candelariaSobol <candee_sobol@hotmail.com>
2023-04-16 22:28:06 -0300

    Chat con websocket

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,14 @@ import { MascotasModule } from './mascotas/mascotas.module';
 import { TurnosModule } from './turnos/turnos.module';
 import { HistorialModule } from './historial/historial.module';
 import { SoapModule } from 'nestjs-soap';
-
+import { GatewayModule } from './gateway/gateway.module';
+import * as cors from 'cors';
 @Module({
   imports: [
     SoapModule.register(
       { clientName: 'MY_SOAP_CLIENT', uri: 'https://www.dataaccess.com/webservicesserver/NumberConversion.wso?wsdl' },
     ),
-    MongooseModule.forRoot('mongodb+srv://candelariasobol:8denoviembre@mongodb101.hkeu0q5.mongodb.net/test'), UserModule, MascotasModule, TurnosModule, HistorialModule],
+    MongooseModule.forRoot('mongodb+srv://candelariasobol:8denoviembre@mongodb101.hkeu0q5.mongodb.net/test'), UserModule, MascotasModule, TurnosModule, HistorialModule, GatewayModule],
   controllers: [AppController],
   providers: [AppService],
 })
