63be2fc3af72adfc8b0e36b4f68b8fb8c68f1651
candelariaSobol <candee_sobol@hotmail.com>
2023-04-19 11:35:15 -0300

    agrego comentarios

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -2,19 +2,19 @@ import { OnModuleInit } from "@nestjs/common";
 import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import {Server} from 'socket.io'
 
-@WebSocketGateway({cors: {origin: '*'}})
+@WebSocketGateway({cors: {origin: '*'}}) //habilitamos los cors
 export class MyGateway implements OnModuleInit{
     @WebSocketServer()
     server: Server
 
-    onModuleInit() {
+    onModuleInit() { //listener en la conexion
         this.server.on('connection', (socket) => {
             console.log(socket.id);
             console.log('Connected');
         })
     }
 
-    @SubscribeMessage('newMessage')
+    @SubscribeMessage('newMessage') //Envio de mensaje
     onNewMessage(@MessageBody() body: any){
         console.log(body);
         this.server.emit('onMessage', {
